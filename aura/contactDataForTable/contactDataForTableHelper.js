341d38524e053274c7c9a647852a47f604e39c35
Andrey <aap.male@gmail.com>
2021-10-12 16:38:57 +0300

    Update contactDataForTableHelper.js

--- aura/contactDataForTable/contactDataForTableHelper.js
+++ aura/contactDataForTable/contactDataForTableHelper.js
@@ -1,23 +1,5 @@
 ({
     getData : function(cmp) {
-        var action = cmp.get('c.auraGetContact');
-        action.setCallback(this, $A.getCallback(function (response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                var record = response.getReturnValue();
-                record.forEach(function(record){
-                    record.urlName = 'https://' + location.host + '/lightning/r/Contact/' + record.Id + '/view';
-                    record.urlAccount = 'https://' + location.host + '/lightning/r/Account/' + record.AccountId + '/view';
-                    record.AccName = record.Account.Name;
-                });
-                cmp.set('v.parentData', record);
-                cmp.set("v.allData", record);
-
-            } else if (state === "ERROR") {
-                var errors = response.getError();
-                console.error(errors);
-            }
-        }));
-        $A.enqueueAction(action);
+        
     }
 })
